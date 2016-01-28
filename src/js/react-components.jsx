57e2de0abac4094cb64319702acbfe92eaf55cb3
beck410 <beck410@hotmail.com>
2016-01-28 15:50:46 -0600

    hook up firebase

--- src/js/react-components.jsx
+++ src/js/react-components.jsx
@@ -3,14 +3,14 @@ var ReactDOM = require('react-dom');
 
 var Comment = React.createClass({
     render: function() {
-    return (
-      <div className="comment">
-        <h2 className="commentAuthor">
-          {this.props.author}
-        </h2>
-        {this.props.children}
-      </div>
-    );
+        return (
+          <div className="comment">
+            <h2 className="commentAuthor">
+              {this.props.author}
+            </h2>
+            {this.props.children}
+          </div>
+        );
     }
 });
 
@@ -18,7 +18,7 @@ var CommentList = React.createClass({
     render: function() {
         var commentNodes = this.props.data.map(comment => {
                 return (
-                    <Comment auther={comment.author} key={comment.id}>
+                    <Comment author={comment.author} key={comment.id}>
                         {comment.text}
                     </Comment>
                 )
@@ -32,27 +32,77 @@ var CommentList = React.createClass({
 });
 
 var CommentForm = React.createClass({
+    getInitialState: function() {
+        return ({author: '', text: ''})
+    },
+
+    handleAuthorChange: function(e) {
+        this.setState({author: e.target.value});
+    },
+
+    handleTextChange: function(e) {
+        this.setState({text: e.target.value});
+    },
+
+    handleSubmit: function(e) {
+        e.preventDefault();
+        var author = this.state.author.trim(),
+            text = this.state.text.trim();
+
+        if(!text || !author) {
+            return;
+        }
+
+        this.props.onCommentSubmit({author: author, text: text});
+        this.setState({author: '', text: ''})
+    },
+
     render: function() {
         return (
-            <div>
-                This is a comment form
-            </div>
+            <form className="comment-form" onSubmit={this.handleSubmit}>
+                <input type="text" placeholder="your name" value={this.state.author} onChange={this.handleAuthorChange} />
+                <input type="text" placeholder="say something..." value={this.state.text} onChange={this.handleTextChange} />
+                <input type="submit" value="Post" />
+            </form>
         )
     }
 })
 
 var CommentBox = React.createClass({
+
+    handleCommentSubmit: function(comment) {
+        this.postComment(comment);
+    },
+
+    postComment: function(comment) {
+        this.firebaseRef.child('comments').push({
+            author: comment.author,
+            text: comment.text
+        }, (data) => {
+            console.log('pushed')
+        })
+    },
+
+    objectAsArray: function(obj) {
+        var index = 0
+        var newArray = [];
+
+        for(let key in obj) {
+            newArray[index] = {}
+            newArray[index]['key'] = key
+            newArray[index]['author'] = obj[key]['author']
+            newArray[index]['text'] = obj[key]['text']
+            index ++
+        }
+
+        index = 0;
+        return newArray;
+    },
+
     loadComments: function() {
-        $.ajax({
-            url: this.props.url,
-            dataType: 'json',
-            cache: false,
-            success: data => {
-                this.setState({data: data})
-            },
-            error: (xhr, status, err) => {
-                console.error(this.props.url, status, err.toString());
-            }
+        this.firebaseRef = new Firebase("https://reacttut1234.firebaseio.com/");
+        this.firebaseRef.child('comments').on("value", comments => {
+            this.setState({data: this.objectAsArray(comments.val())})
         })
     },
 
@@ -69,7 +119,7 @@ var CommentBox = React.createClass({
             <div className="comment-box">
                 <h1>Comments</h1>
                 <CommentList data={this.state.data}/>
-                <CommentForm />
+                <CommentForm onCommentSubmit={this.handleCommentSubmit}/>
             </div>
         )
     }
