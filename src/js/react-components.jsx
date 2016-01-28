252920ffd217fa3fab5d5b386114ccd3d5789138
beck410@hotmail.com <beck@eventbrite.com>
2016-01-28 11:32:10 -0600

    set up nested components

--- src/js/react-components.jsx
+++ src/js/react-components.jsx
@@ -1,15 +1,53 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var HelloWorld = React.createClass({
+var Comment = React.createClass({
+    render: function() {
+    return (
+      <div className="comment">
+        <h2 className="commentAuthor">
+          {this.props.author}
+        </h2>
+        {this.props.children}
+      </div>
+    );
+    }
+});
+
+var CommentList = React.createClass({
     render: function() {
         return (
-            <h1>Hello, world!</h1>
+            <div className="comment-list">
+                <Comment author="Beck">This is one comment</Comment>
+                <Comment author="Dave">This is another comment</Comment>
+            </div>
+        )
+    }
+});
+
+var CommentForm = React.createClass({
+    render: function() {
+        return (
+            <div>
+                This is a comment form
+            </div>
         )
     }
 })
 
+var CommentBox = React.createClass({
+    render: function() {
+        return (
+            <div className="comment-box">
+                <h1>Comments</h1>
+                <CommentList />
+                <CommentForm />
+            </div>
+        )
+    }
+});
+
 ReactDOM.render(
-  <HelloWorld />,
-  document.getElementById('example')
+  <CommentBox />,
+  document.getElementById('react-comments')
 );
