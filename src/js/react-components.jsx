7b989963607827bfee778234a8fc80fd5414639e
beck410 <beck410@hotmail.com>
2016-01-29 12:08:17 -0600

    added update functionality

--- src/js/react-components.jsx
+++ src/js/react-components.jsx
@@ -5,37 +5,46 @@ var Comment = React.createClass({
     render: function() {
         return (
           <div className="comment">
-            <h2 className="commentAuthor">
-              {this.props.author}
-            </h2>
+            <h2 className="commentAuthor">{this.props.author}</h2>
             {this.props.children}
-            <div key={this.props.key} onClick={this.props.onDelete.bind(null, this.props.id)} className="delete">Delete</div>
+            <div onClick={this.props.onDelete.bind(null, this.props.id)} className="delete">Delete</div>
+            <div onClick={this.props.onEdit.bind(null, this.props)} className="edit">Edit</div>
           </div>
         );
     }
 });
 
 var CommentList = React.createClass({
-    render: function() {
-        var onDelete = this.props.onDelete,
-            commentNodes = this.props.data.map(comment => {
+    buildCommentNodes: function() {
+        return this.props.data.map(comment => {
+            if (comment.editing == false) {
                 return (
-                    <Comment onDelete={onDelete} id={comment.key} key={comment.key} author={comment.author} >
+                    <Comment onEdit={this.props.onEdit} onDelete={this.props.onDelete} id={comment.key} key={comment.key} author={comment.author} >
                         {comment.text}
                     </Comment>
                 )
-            })
-            return (
-                <div className="comment-list">
-                    {commentNodes}
-                </div>
-            )
-        }
+            } else {
+                return (
+                    <CommentForm onCommentSubmit={this.props.editSubmit} id={comment.key} author={comment.author} text={comment.text} editing={comment.editing}></CommentForm>
+                )
+            }
+        });
+    },
+
+    render: function() {
+        var commentNodes = this.buildCommentNodes();
+        return (
+            <div className="comment-list">
+                {commentNodes}
+            </div>
+        )
+    }
 });
 
 var CommentForm = React.createClass({
-    getInitialState: function() {
-        return ({author: '', text: ''})
+
+    submitBtnValue: function() {
+        return this.state.editing ? "Edit" : "Post";
     },
 
     handleAuthorChange: function(e) {
@@ -44,6 +53,15 @@ var CommentForm = React.createClass({
 
     handleTextChange: function(e) {
         this.setState({text: e.target.value});
+    },
+
+    getInitialState: function() {
+        return {
+            id: this.props.id || null,
+            author: this.props.author || '',
+            text: this.props.text || '',
+            editing: this.props.editing || false
+        }
     },
 
     handleSubmit: function(e) {
@@ -55,7 +73,7 @@ var CommentForm = React.createClass({
             return;
         }
 
-        this.props.onCommentSubmit({author: author, text: text});
+        this.props.onCommentSubmit({author: author, text: text}, this.state.editing, this.state.id);
         this.setState({author: '', text: ''})
     },
 
@@ -64,7 +82,7 @@ var CommentForm = React.createClass({
             <form className="comment-form" onSubmit={this.handleSubmit}>
                 <input type="text" placeholder="your name" value={this.state.author} onChange={this.handleAuthorChange} />
                 <input type="text" placeholder="say something..." value={this.state.text} onChange={this.handleTextChange} />
-                <input type="submit" value="Post" />
+                <input type="submit" value={this.submitBtnValue()} />
             </form>
         )
     }
@@ -72,11 +90,37 @@ var CommentForm = React.createClass({
 
 var CommentBox = React.createClass({
 
+    editComment: function(data, id) {
+        this.firebaseRef.child('comments/' + id).update({
+            author: data.author,
+            text: data.text
+        }, data => {
+            console.log('edited');
+        })
+    },
+
+    handleEditComment: function(data) {
+
+        var newState = this.state.data.map(comment => {
+            if(comment.key == data.id) {
+                comment['editing'] = true;
+                comment['author'] = data.author;
+                comment['text'] = data.children;
+            }
+            return comment;
+        })
+        this.setState({data: newState})
+    },
+
     handleDeleteComment: function(id) {
         this.firebaseRef.child('comments/' + id).remove()
     },
 
-    handleCommentSubmit: function(comment) {
+    handleCommentSubmit: function(comment, isEditMode, id) {
+        if(isEditMode) {
+            this.editComment(comment, id);
+            return;
+        }
         this.postComment(comment);
     },
 
@@ -94,11 +138,12 @@ var CommentBox = React.createClass({
         var newArray = [];
 
         for(let key in obj) {
-            newArray[index] = {}
-            newArray[index]['key'] = key
-            newArray[index]['author'] = obj[key]['author']
-            newArray[index]['text'] = obj[key]['text']
-            index ++
+            newArray[index] = {};
+            newArray[index]['key'] = key;
+            newArray[index]['author'] = obj[key]['author'];
+            newArray[index]['text'] = obj[key]['text'];
+            newArray[index]['editing'] = false;
+            index ++;
         }
 
         index = 0;
@@ -108,7 +153,7 @@ var CommentBox = React.createClass({
     loadComments: function() {
         this.firebaseRef = new Firebase("https://reacttut1234.firebaseio.com/");
         this.firebaseRef.child('comments').on("value", comments => {
-            this.setState({data: this.objectAsArray(comments.val())})
+            this.setState({data: this.objectAsArray(comments.val())});
         })
     },
 
@@ -124,8 +169,8 @@ var CommentBox = React.createClass({
         return (
             <div className="comment-box">
                 <h1>Comments</h1>
-                <CommentList onDelete={this.handleDeleteComment} data={this.state.data}/>
-                <CommentForm onCommentSubmit={this.handleCommentSubmit}/>
+                <CommentList editSubmit={this.handleCommentSubmit} onEdit={this.handleEditComment} onDelete={this.handleDeleteComment} data={this.state.data}/>
+                <CommentForm editing={false} onCommentSubmit={this.handleCommentSubmit} />
             </div>
         )
     }
